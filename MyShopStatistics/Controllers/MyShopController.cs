fea0da739d6cb7b80221f945972f412a3c7e3418
Jacob Pagel <jpagel@ancestry.com>
2014-08-08 21:51:09 -0600

    InitialSync

---
+++ MyShopStatistics/Controllers/MyShopController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShopStatistics.Controllers
+{
+    public class MyShopController : Controller
+    {
+
+        // GET: MyShop
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        // GET: MyShop
+        public ActionResult ShopStatsHome()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyShopStatistics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShopStatistics.Startup))]
+namespace MyShopStatistics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
